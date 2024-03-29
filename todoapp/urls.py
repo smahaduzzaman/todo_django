14c82797c1a482f0b95f45ce3bc9ab48fc2d74bf
S M Ahaduzzaman <smahaduzzaman96@gmail.com>
2024-03-30 02:07:34 +0600

    Template mastering, urls, form, active link and todo page form create

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index , about, addtodo
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('addtodo/', addtodo, name='addtodo')
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'todoapp/index.html')
+
+def about(request):
+    return render(request, 'todoapp/about.html')
+
+def addtodo(request):
+    return render(request, 'todoapp/addtodo.html')
