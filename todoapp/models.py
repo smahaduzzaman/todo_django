97f37e7cff44ee140a07debbf3e9e98f89c29d14
S M Ahaduzzaman <smahaduzzaman96@gmail.com>
2024-03-30 08:31:40 +0600

    Create todo and read it on the addtodo page

--- todoapp/models.py
+++ todoapp/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
-        
-        
-        
+    
+    class Meta:
+        ordering = ['-created_at',]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
@@ -8,4 +9,13 @@ def about(request):
     return render(request, 'todoapp/about.html')
 
 def addtodo(request):
-    return render(request, 'todoapp/addtodo.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        todo = Todo.objects.create(title=title)
+        todo.save()
+        return render(request, 'todoapp/addtodo.html', {'message': 'Todo added successfully!'})
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todoapp/addtodo.html', context)
