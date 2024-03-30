ad0382689840f4082453e29e852eb1b330f4170f
S M Ahaduzzaman <smahaduzzaman96@gmail.com>
2024-03-30 12:33:53 +0600

    Create Read and Delete is completed

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index , about, addtodo
+from .views import index , about, addtodo, updatetodo, deletetodo
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
-    path('addtodo/', addtodo, name='addtodo')
+    path('addtodo/', addtodo, name='addtodo'),
+    path('updatetodo/<int:id>/', updatetodo, name='updatetodo'),
+    path('deletetodo/<int:id>/', deletetodo, name='deletetodo')
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -11,11 +11,34 @@ def about(request):
 def addtodo(request):
     if request.method == 'POST':
         title = request.POST['title']
+        if not title:
+            return render(request, 'todoapp/addtodo.html', {'message': 'Please enter a title!'})
         todo = Todo.objects.create(title=title)
         todo.save()
-        return render(request, 'todoapp/addtodo.html', {'message': 'Todo added successfully!'})
+        return redirect('addtodo')
+        
+    
     todos = Todo.objects.all()
     context = {
         'todos': todos
     }
     return render(request, 'todoapp/addtodo.html', context)
+
+def updatetodo(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method == 'POST':
+        title = request.POST['title']
+        if not title:
+            return render(request, 'todoapp/updatetodo.html', {'todo': todo, 'message': 'Please enter a title!'})
+        todo.title = title
+        todo.save()
+        return render(request, 'todoapp/updatetodo.html', {'todo': todo, 'message': 'Todo updated successfully!'})
+    context = {
+        'todo': todo
+    }
+    return render(request, 'todoapp/updatetodo.html', context)
+    
+def deletetodo(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('addtodo')
